8720e90ab79cb76a3b4c380765ae79b6940af1de
Tim Stahlhut <stahta01@gmail.com>
2021-01-10 15:17:29 -0500

    cb_share_config: Remove include of wx.h or wxprec.h from 1 header file

--- src/tools/cb_share_config/app.h
+++ src/tools/cb_share_config/app.h
@@ -6,16 +6,6 @@
 #ifndef APP_H
 #define APP_H
 
-#include <wx/wxprec.h>
-
-#ifdef __BORLANDC__
-	#pragma hdrstop
-#endif
-
-#ifndef WX_PRECOMP
-	#include <wx/wx.h>
-#endif
-
 class MyApp : public wxApp
 {
 	public:
